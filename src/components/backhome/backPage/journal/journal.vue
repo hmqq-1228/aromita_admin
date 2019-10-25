bb085398901a5fb22df9cfc2f973f137e220fe43
zhuyabei <837765980@qq.com>
2019-10-25 16:06:41 +0800

    日志管理更新

--- src/components/backhome/backPage/journal/journal.vue
+++ src/components/backhome/backPage/journal/journal.vue
@@ -8,8 +8,9 @@
             style="width: 100%"
             max-height="780px">
             <el-table-column prop="oper_name" label="操作者"></el-table-column>
+            <el-table-column prop="created_at" label="操作时间"></el-table-column>
+            <el-table-column prop="oper_ip" label="IP"></el-table-column>
             <el-table-column prop="log_channel" label="日志所在模块"></el-table-column>
-            <el-table-column prop="log_extra" label="日志扩展信息"></el-table-column>
             <el-table-column label="日志内容" width="500px">
                 <template slot-scope="scope">
                    <span class="content">
@@ -17,12 +18,17 @@
                    </span>
                 </template>
             </el-table-column>
-            <!-- <el-table-column label="操作">
-                <template slot-scope="scope">
-                    <el-button type="primary" @click="viewDetail(scope.row.id)">查看详情</el-button>
-                </template>
-            </el-table-column> -->
+            <el-table-column prop="log_extra" label="日志扩展信息"></el-table-column>
         </el-table>
+        <div class="pagination">
+            <el-pagination
+                background
+                :total="total"
+                layout="prev, pager, next"
+                :page-size="50"
+                @current-change="changePage">
+            </el-pagination>
+        </div>
     </div>
 </template>
 <script>
@@ -30,6 +36,8 @@ import {getJournalList} from '@/http/journal.js'
 export default {
     data(){
         return{
+            total:0,
+            page:1,
             journalList:[]
         }
     },
@@ -38,11 +46,16 @@ export default {
     },
     methods:{
         getList(){
-            getJournalList().then((res)=>{
+            getJournalList({page:this.page}).then((res)=>{
                 if(res.data.code == 200){
                     this.journalList = res.data.data.data
+                    this.total = res.data.data.total
                 }
             })
+        },
+        changePage(val){
+            this.page = val
+            this.getList()
         },
         viewDetail(id){
             this.$router.push({path:'/journaldetail',query:{id:id}})
