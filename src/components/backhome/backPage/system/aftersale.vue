6ee9702545437a73c7c14975fb227e62b883a8e0
zhuyabei <837765980@qq.com>
2019-10-29 08:40:08 +0800

    售后列表

---
+++ src/components/backhome/backPage/system/aftersale.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="aftersale">
+        售后列表
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    created(){
+
+    }
+}
+</script>
+<style scoped>
+    
+</style>
