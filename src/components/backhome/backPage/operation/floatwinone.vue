93474d050daddde808ca2a5e936a81a6cb0f4aaa
zhuyabei <837765980@qq.com>
2019-11-27 10:15:59 +0800

    广告页面完善

--- src/components/backhome/backPage/operation/floatwinone.vue
+++ src/components/backhome/backPage/operation/floatwinone.vue
@@ -30,46 +30,110 @@
                     <el-input></el-input>
                 </el-form-item>
                 <el-form-item label="广告背景：">
-
+                    <el-upload
+                        class="avatar-uploader"
+                        :action="uploadUrl"
+                        :data="mainType"
+                        name="image"
+                        accept=".jpg,.jpeg,.png,.JPG,.JPEG"
+                        :show-file-list="false"
+                        :on-success="handleAvatarSuccess"
+                        :before-upload="beforeAvatarUpload">
+                        <img v-if="adverteform.backimg" :src="adverteform.backimg" class="avatar">
+                        <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                    </el-upload>
                 </el-form-item>
                 <el-form-item label="是否开启倒计时：">
-                    
+                    <el-radio-group v-model="adverteform.time">
+                        <el-radio :label="1">是</el-radio>
+                        <el-radio :label="0">否</el-radio>
+                    </el-radio-group>
                 </el-form-item>
                 <el-form-item label="广告位置：">
-                    
+                    <el-radio-group v-model="adverteform.position">
+                        <el-radio :label="1">左对齐</el-radio>
+                        <el-radio :label="2">居中</el-radio>
+                        <el-radio :label="3">右对齐</el-radio>
+                    </el-radio-group>
                 </el-form-item>
                 <el-form-item label="广告详情：">
-                    
+                    <el-upload
+                        class="avatar-uploader"
+                        :action="uploadUrl"
+                        :data="mainType"
+                        name="image"
+                        accept=".jpg,.jpeg,.png,.JPG,.JPEG"
+                        :show-file-list="false"
+                        :on-success="handleAvatarSuccess"
+                        :before-upload="beforeAvatarUpload">
+                        <img v-if="adverteform.detailimg" :src="adverteform.detailimg" class="avatar">
+                        <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                    </el-upload>
                 </el-form-item>
                 <el-form-item label="广告链接：">
                     <el-input></el-input>
                 </el-form-item>
                 <el-form-item label="是否默认广告：">
-                    
+                    <el-radio-group v-model="adverteform.default">
+                        <el-radio :label="1">是</el-radio>
+                        <el-radio :label="0">否</el-radio>
+                    </el-radio-group>
                 </el-form-item>
                 <el-form-item label="广告状态：">
-                    
+                    <el-switch
+                        v-model="adverteform.switch">
+                    </el-switch>
                 </el-form-item>
                 <el-form-item>
                     <el-button type="primary">保存</el-button>
-                    <el-button type="danger">取消</el-button>
+                    <el-button type="danger" @click="cancel()">取消</el-button>
                 </el-form-item>
             </el-form>
         </div>
     </div>
 </template>
 <script>
+import {uploadUrl} from '@/http/commodity.js'
 export default {
     data(){
         return{
-            adverteform:{}
+            //上传商品背景图
+            uploadUrl:uploadUrl,
+            mainType:{
+                type:'main'
+            },
+            adverteform:{
+                backimg:'',
+                detailimg:'',
+                position:1,
+                time:1,
+                default:0,
+                switch:''
+            },
         }
     },
     created(){
 
     },
     methods:{
+        beforeAvatarUpload(){
 
+        },
+        handleAvatarSuccess(){
+
+        },
+        //取消广告设置
+        cancel(){
+            this.$confirm('广告设置还没有保存，确定取消吗?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+                this.$router.push({path:'/advertising'})
+            }).catch(() => {
+                          
+            });
+        }
     }
 }
 </script>
@@ -87,7 +151,7 @@ export default {
 .floatwinone .header a{
     margin-right: 20px;
 }
-.demo-form-inline .el-input{
+.floatwinone .demo-form-inline .el-input{
     width: 400px!important;
 }
 </style>
--- src/components/backhome/backPage/operation/floatwintwo.vue
+++ src/components/backhome/backPage/operation/floatwintwo.vue
@@ -10,19 +10,40 @@
             </router-link>
         </div>
         <div class="orderCenter">
-            <el-form :model="adverteform" class="demo-form-inline">
-                <el-form-item label="状态："></el-form-item>
-                <el-form-item label="广告时段：">
-                    <el-date-picker
-                        v-model="adverteform.time"
-                        type="datetimerange"
-                        range-separator="至"
-                        start-placeholder="开始日期"
-                        end-placeholder="结束日期">
-                    </el-date-picker>
+            <el-form :model="adverteform" class="demo-form-inline" label-width="160px">
+                <el-form-item label="广告名称（后台）：" required>
+                    <el-input clearable placeholder="请输入广告名称/广告标题"></el-input>
+                </el-form-item>
+                <el-form-item label="订阅福利：" required>
+                    <el-radio-group v-model="adverteform.radio">
+                        <div class="radioBox">
+                            <el-radio :label="1">送优惠券</el-radio>
+                            <el-select placeholder="请选择优惠券类型" v-model="adverteform.radiovalue">
+                                <el-option
+                                    v-for="item in options"
+                                    :key="item.value"
+                                    :label="item.label"
+                                    :value="item.value">
+                                    </el-option>
+                            </el-select>
+                        </div>
+                        <div class="radioBox">
+                            <el-radio :label="2">送积分</el-radio>
+                            <el-input placeholder="请输入积分点数"></el-input>
+                        </div>
+                        <div class="radioBox">
+                            <el-radio :label="3">无福利</el-radio>
+                        </div>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item label="状态：" required>
+                    <el-switch
+                        v-model="adverteform.switch">
+                    </el-switch>
                 </el-form-item>
                 <el-form-item>
-                    <el-input clearable placeholder="请输入广告名称/广告标题"></el-input>
+                    <el-button type="primary">保存</el-button>
+                    <el-button type="danger" @click="cancel()">取消</el-button>
                 </el-form-item>
             </el-form>
         </div>
@@ -33,15 +54,45 @@ export default {
     data(){
         return{
             adverteform:{
-
-            }
+                radio:1,
+                radiovalue:'',
+                switch:''
+            },
+            options: [{
+                value: '选项1',
+                label: '黄金糕'
+                }, {
+                value: '选项2',
+                label: '双皮奶'
+                }, {
+                value: '选项3',
+                label: '蚵仔煎'
+                }, {
+                value: '选项4',
+                label: '龙须面'
+                }, {
+                value: '选项5',
+                label: '北京烤鸭'
+            }],
+            value: ''
         }
     },
     created(){
 
     },
     methods:{
-
+        //取消广告设置
+        cancel(){
+            this.$confirm('广告设置还没有保存，确定取消吗?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+                this.$router.push({path:'/advertising'})
+            }).catch(() => {
+                          
+            });
+        }
     }
 }
 </script>
@@ -58,5 +109,13 @@ export default {
 }
 .floatwinone .header a{
     margin-right: 20px;
+}
+.floatwinone .demo-form-inline .el-input{
+    width: 400px!important;
+}
+.floatwinone .radioBox{
+    height: 50px;
+    display: flex;
+    align-items:center;
 }
 </style>
